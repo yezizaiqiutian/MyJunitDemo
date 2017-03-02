23fd147ba9f3b477db9fdb6a180e9a55d4edbf42
yezizaiqiutian <15652919252@163.com>
2017-03-02 18:56:05 +0800

    Initial commit

---
+++ app/src/main/java/com/gh/myjunitdemo/Calculator.java
@@ -0,0 +1,27 @@
+package com.gh.myjunitdemo;
+
+/**
+ * @author: gh
+ * @description: TODO(描述)
+ * @date: 2017/3/2 17:25
+ * @note:
+ */
+
+public class Calculator {
+
+    public double sum(double a, double b){
+        return 0;
+    }
+
+    public double substract(double a, double b){
+        return 0;
+    }
+
+    public double divide(double a, double b){
+        return 0;
+    }
+
+    public double multiply(double a, double b){
+        return 0;
+    }
+}
